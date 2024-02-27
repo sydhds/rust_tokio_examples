f6751a0e799b47560a9177dc4ead431de2f727c5
sydhds <sydhds@gmail.com>
2024-02-27 10:56:10 +0100

    Use anyhow in rayon example

--- rust_crate_rayon_01/src/main.rs
+++ rust_crate_rayon_01/src/main.rs
@@ -12,18 +12,17 @@ fn main() {
     let width = 320;
     let height = 240;
     let path_1 = PathBuf::from("image.ppm");
-    let path_2 = PathBuf::from("image_gray.ppm");
+    let path_2_1 = PathBuf::from("image_gray_std.ppm");
+    let path_2_2 = PathBuf::from("image_gray_rayon.ppm");
 
     println!("Generating image data...");
-    let mut img = generate_image_data(width, height);
+    let mut img = generate_image_data(width, height).expect("Unable to generate image data");
 
     println!("Writing {:?}...", path_1);
-    write_image(width, height, &img, path_1);
-
+    write_image(width, height, &img, &path_1)
+        .expect(format!("Unable to write image {:?}", path_1).as_str());
     let mut img2 = img.clone();
-    // std chunks iterator
-    let st = std::time::Instant::now();
-    img2.chunks_exact_mut(3).for_each(|chunk| {
+    let to_grayscale = |chunk: &mut [u8]| {
         let r = chunk[0];
         let g = chunk[1];
         let b = chunk[2];
@@ -34,7 +33,11 @@ fn main() {
         chunk[0] = gray_ as u8;
         chunk[1] = gray_ as u8;
         chunk[2] = gray_ as u8;
-    });
+    };
+
+    // std chunks iterator
+    let st = std::time::Instant::now();
+    img.chunks_exact_mut(3).for_each(to_grayscale);
     let elapsed = st.elapsed();
     println!(
         "std chunks exact mut - elapsed {} microseconds",
@@ -43,31 +46,25 @@ fn main() {
 
     // par_chunks is a // rayon iterator
     let st2 = std::time::Instant::now();
-    img.par_chunks_exact_mut(3).for_each(|chunk| {
-        let r = chunk[0];
-        let g = chunk[1];
-        let b = chunk[2];
-
-        // rgb to grayscale using luminosity method
-        let gray_ = 0.3 * f64::from(r) + 0.59 * f64::from(g) + 0.11 * f64::from(b);
-
-        chunk[0] = gray_ as u8;
-        chunk[1] = gray_ as u8;
-        chunk[2] = gray_ as u8;
-    });
+    img2.par_chunks_exact_mut(3).for_each(to_grayscale);
     let elapsed2 = st2.elapsed();
     println!(
         "rayon chunks exact mut - elapsed {} microseconds",
         elapsed2.as_micros()
     );
 
-    println!("Writing {:?}...", path_2);
-    write_image(width, height, &img, path_2);
+    println!("Writing {:?}...", path_2_1);
+    write_image(width, height, &img, &path_2_1)
+        .expect(format!("Unable to write image {:?}", path_2_1).as_str());
+
+    println!("Writing {:?}...", path_2_2);
+    write_image(width, height, &img2, &path_2_2)
+        .expect(format!("Unable to write image {:?}", path_2_2).as_str());
 }
 
-fn generate_image_data(width: u16, height: u16) -> Vec<u8> {
+fn generate_image_data(width: u16, height: u16) -> anyhow::Result<Vec<u8>> {
     // let data_size = width * height * 3;
-    let data_size = usize::try_from(u32::from(width) * u32::from(height) * 3).unwrap();
+    let data_size = usize::try_from(u32::from(width) * u32::from(height) * 3)?;
     eprintln!("Image size: {}", data_size);
     let mut data: Vec<u8> = Vec::with_capacity(data_size);
 
@@ -87,29 +84,31 @@ fn generate_image_data(width: u16, height: u16) -> Vec<u8> {
         }
     }
 
-    return data;
+    Ok(data)
 }
 
-fn write_image(width: u16, height: u16, data: &Vec<u8>, path: PathBuf) {
-    let file = std::fs::File::create(path).unwrap();
+fn write_image(width: u16, height: u16, data: &Vec<u8>, path: &PathBuf) -> anyhow::Result<()> {
+    let file = std::fs::File::create(path)?;
     let mut writer = LineWriter::new(file);
 
     let color_count = 3;
     let line_size = u32::from(width) * color_count;
 
-    writer.write_all(b"P3\n").unwrap();
-    write!(writer, "{} {}\n", width, height).unwrap();
-    writer.write_all(b"255\n").unwrap();
+    writer.write_all(b"P3\n")?;
+    write!(writer, "{} {}\n", width, height)?;
+    writer.write_all(b"255\n")?;
 
     for j in 0..height {
         for i in 0..width {
             let index_ = (u32::from(j) * line_size) + (u32::from(i) * 3);
-            let index = usize::try_from(index_).unwrap();
+            let index = usize::try_from(index_)?;
 
             let r = data[index];
             let g = data[index + 1];
             let b = data[index + 2];
-            write!(writer, "{} {} {}\n", r, g, b).unwrap();
+            write!(writer, "{} {} {}\n", r, g, b)?;
         }
     }
+
+    Ok(())
 }
